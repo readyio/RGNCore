724aaecdd21f8781d918314819e924eb8a3788b2
Konstantin Gindemit <konstantin@getready.io>
2023-03-20 12:17:04 +0000

    New package version for Core, version: 0.1.0-dev.123

--- Runtime/src/UI/RGNButton.cs
+++ Runtime/src/UI/RGNButton.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 namespace RGN.UI
 {
     [RequireComponent(typeof(Button), typeof(RectTransform))]
-    public sealed class RGNButton : MonoBehaviour
+    public sealed class RGNButton : MonoBehaviour, System.IDisposable
     {
         public RectTransform RectTransform { get => _rectTransform; }
         public Button Button { get => _button; }
@@ -15,9 +15,23 @@ namespace RGN.UI
         [SerializeField] private Button _button;
         [SerializeField] private TextMeshProUGUI _buttonText;
 
+        public void Init()
+        {
+
+        }
+        public void Dispose()
+        {
+            Destroy(gameObject);
+        }
         public float GetHeight()
         {
             return RectTransform.sizeDelta.y;
+        }
+        public void SetHeight(float height)
+        {
+            RectTransform.sizeDelta = new Vector2(
+                RectTransform.sizeDelta.x,
+                height);
         }
     }
 }
