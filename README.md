c374c9e947dbf281a0817789b8112dcfa93552c9
Git GetReady <git@getready.io>
2024-03-11 20:10:03 +0000

    New package version for Core, version: 0.9.2

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # RGN Core
-Package version: 0.9.1
+Package version: 0.9.2
--- Runtime/src/SDKVersion.cs
+++ Runtime/src/SDKVersion.cs
@@ -2,6 +2,6 @@ namespace RGN
 {
     public static class SDKVersion
     {
-        public static string Version = "0.9.1";
+        public static string Version = "0.9.2";
     }
 }
