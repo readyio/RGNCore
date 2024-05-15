a505fea6954d548e26d742453abf0402b6dabc2c
Git GetReady <git@getready.io>
2024-05-15 15:10:11 +0000

    New package version for Core, version: 0.11.0-dev.549

--- Runtime/src/SDKVersion.cs
+++ Runtime/src/SDKVersion.cs
@@ -2,6 +2,6 @@ namespace RGN
 {
     public static class SDKVersion
     {
-        public static string Version = "0.11.0-dev.548";
+        public static string Version = "0.11.0-dev.549";
     }
 }
