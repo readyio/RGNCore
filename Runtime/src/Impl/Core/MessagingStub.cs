791cec90197eb9bd2319abf7f2930b025e20702e
Git GetReady <git@getready.io>
2024-11-27 11:19:26 +0000

    New package version for Core, version: 0.16.0-dev.623

--- Runtime/src/Impl/Core/MessagingStub.cs
+++ Runtime/src/Impl/Core/MessagingStub.cs
@@ -1,6 +1,7 @@
 using RGN.ImplDependencies.Core.Messaging;
 using System;
 using RGN.ImplDependencies.Core;
+using System.Threading.Tasks;
 
 namespace RGN.Impl.Firebase.Core
 {
@@ -9,5 +10,25 @@ namespace RGN.Impl.Firebase.Core
         public event Action<object, ITokenReceivedEventArgs> TokenReceived { add { } remove { } }
 
         public event Action<object, IMessageReceivedEventArgs> MessageReceived { add { } remove { } }
+        public Task TopicSubscribeAsync(string topic)
+        {
+            return Task.CompletedTask;
+        }
+        public Task TopicUnsubscribeAsync(string topic)
+        {
+            return Task.CompletedTask;
+        }
+        public Task RequestPermissionAsync()
+        {
+            return Task.CompletedTask;
+        }
+        public Task<string> GetTokenAsync()
+        {
+            return Task.FromResult(string.Empty);
+        }
+        public Task DeleteTokenAsync()
+        {
+            return Task.CompletedTask;
+        }
     }
 }
